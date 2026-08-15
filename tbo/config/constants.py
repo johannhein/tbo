7937bdcdd6bd5e46127bad3e0c6cbed79e4984e4
hein_j5 <johann.heinke@dlr.de>
2026-08-15 14:13:19 +0200

    add new constants

--- tbo/config/constants.py
+++ tbo/config/constants.py
@@ -6,10 +6,12 @@ IMG_PATH = Path("tbo/assets/header.jpg")
 
 DATA_DIR = Path("tbo/data")
 IMPORT_DIR = Path("tbo/import")
+TEMPLATE_DIR = Path("tbo/templates")
+EXPORT_DIR = Path("tbo/export")
 
 TOURNAMENT_NAME = "19. Travemünder Beach Open  07./ 08. August 2027"
 
-# Hard‑coded Group‑Matches – kann später in JSON ausgelagert werden
+# Hard‑coded Group‑Matches –kann später in JSON ausgelagert werden
 GROUP_MATCHES = [
     (1, "09:45-10:00", 1, "A1", "A6", "A"),
     (1, "09:45-10:00", 2, "A2", "A5", "A"),
