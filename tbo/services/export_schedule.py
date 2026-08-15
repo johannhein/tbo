487c9fa036b2a3ca398d2cf6bd544733b3d0d3da
hein_j5 <johann.heinke@dlr.de>
2026-08-15 14:14:06 +0200

    add function to export a whole alle groups from a stage at once

--- tbo/services/export_schedule.py
+++ tbo/services/export_schedule.py
@@ -1,18 +1,28 @@
 from __future__ import annotations
 import pathlib
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
-from core.models import Group, Match
+from config.constants import EXPORT_DIR, TEMPLATE_DIR
+from core.models import Group, Match, Tournament, StageID
+from services.path import ensure_dir
 
 
-def render_group_schedule_html(group: Group,
-                         template_dir: str = "templates") -> pathlib.Path:
+def render_group_schedule_html(group: Group, tournament_type: str = None, template_dir: str = TEMPLATE_DIR) -> pathlib.Path:
     """
     Rendert das Jinja2‑Template `schedule.html` mit den Daten aus `group`
     und schreibt das Ergebnis in `output_path`.
     """
-    file_name = f"Spielplan_Gruppe_{group.name}.html"
-    env = Environment(loader=FileSystemLoader(template_dir), autoescape=True)
+    if tournament_type:
+        export_dir = Path(EXPORT_DIR / tournament_type.lower())
+    else:
+        export_dir = Path(EXPORT_DIR)
+
+    ensure_dir(export_dir)
+
+    file_name =f"Spielplan_Gruppe_{group.name}.html"
+    print(Path(template_dir).absolute())
+    env = Environment(loader=FileSystemLoader(Path(template_dir).absolute()), autoescape=True)
     template = env.get_template("schedule.html")
 
     rendered = template.render(
@@ -20,8 +30,15 @@ def render_group_schedule_html(group: Group,
         matches=group.match_list
     )
 
-    output_path = "tbo/export/" + file_name
-    out_file = pathlib.Path(output_path)
+    out_file = Path(export_dir / file_name)
     out_file.write_text(rendered, encoding="utf-8")
     print(f"✅ HTML‑Datei wurde geschrieben: {out_file.resolve()}")
     return out_file
+
+
+def export_stage(tournament: Tournament, stage_id: StageID):
+    stage = tournament.get_stage(stage_id)
+
+    for group in stage.groups:
+        render_group_schedule_html(group= group, tournament_type=tournament.type)
+
