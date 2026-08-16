fc107b3e1b472e5a912ce3213b9658eec38aa52a
hein_j5 <johann.heinke@dlr.de>
2026-08-16 21:20:02 +0200

    add color for highlighting

--- tbo/config/constants.py
+++ tbo/config/constants.py
@@ -11,6 +11,8 @@ EXPORT_DIR = Path("tbo/export")
 
 TOURNAMENT_NAME = "19. Travemünder Beach Open  07./ 08. August 2027"
 
+HIGHLIGHT_COLOR = "#00ab17"
+
 # Hard‑coded Group‑Matches –kann später in JSON ausgelagert werden
 GROUP_MATCHES = [
     (1, "09:45-10:00", 1, "A1", "A6", "A"),
