e746fae078f7c90429c58a4dd9e06f6db22240f8
hein_j5 <johann.heinke@dlr.de>
2026-08-20 12:01:28 +0200

    add courts numbers to the table

--- tbo/src/config/constants.py
+++ tbo/src/config/constants.py
@@ -8,14 +8,21 @@ PICKLE_DIR = DATA_DIR / "pickle"
 IMPORT_DIR: Path = DATA_DIR /"import"
 TEMPLATE_DIR: Path = ASSETS_DIR / "templates"
 EXPORT_DIR: Path = DATA_DIR / "export"
+DB_PATH : Path = DATA_DIR / "tournament_db.db"
+
+SQL_SCHEMA_PATH: Path = Path("tbo/src/config/schema.sql")
 
 TOURNAMENT_NAME = "19. Travemünder Beach Open  07./ 08. August 2027"
 
 HIGHLIGHT_COLOR = "#00ab17"
 
-# Hard‑coded Group‑Matches –kann später in JSON ausgelagert werden
-GROUP_MATCHES = [
-    (1, "09:45-10:00", 1, "A1", "A6", "A"),
-    (1, "09:45-10:00", 2, "A2", "A5", "A"),
-    # … (alle übrigen Zeilen) …
+MAX_COURT_NUM = 16
+
+TOURNAMENT_DAYS = [
+    ("Damen", "Sonntag", "Damen", "[2, 3, 4, 5]"),
+    ("Herren", "Sonntag", "Herren", "[1, 6, 7, 10, 11, 12, 13, 14]"),
+    ("Quattro", "Sonntag", "Mixed", "[8, 9, 15, 16]"),
+    ("Duo Mixed Hobby", "Samstag", "Mixed",
+     "[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]"),
+    ("Duo Mixed Leistung", "Samstag", "Mixed", "[1, 2, 3, 4]"),
 ]
