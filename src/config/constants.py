21a53e109356fb06f3e0e8d308ace4d34f610ab7
hein_j5 <johann.heinke@dlr.de>
2026-08-23 21:43:39 +0200

    moved add new variables

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
-ASSETS_DIR: Path = Path("tbo/assets")
+ASSETS_DIR: Path = Path("assets")
 IMG_PATH: Path = ASSETS_DIR / "images/header.jpg"
 
-DATA_DIR: Path = Path("tbo/data")
+DATA_DIR: Path = Path("data")
 PICKLE_DIR = DATA_DIR / "pickle"
 IMPORT_DIR: Path = DATA_DIR /"import"
 TEMPLATE_DIR: Path = ASSETS_DIR / "templates"
 EXPORT_DIR: Path = DATA_DIR / "export"
 DB_PATH : Path = DATA_DIR / "tournament_db.db"
 
-SQL_SCHEMA_PATH: Path = Path("tbo/src/config/schema.sql")
+SQL_SCHEMA_PATH: Path = Path("src/config/schema.sql")
 
 TOURNAMENT_NAME = "19. Travemünder Beach Open  07./ 08. August 2027"
 
@@ -26,3 +26,9 @@ TOURNAMENT_DAYS = [
      "[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]"),
     ("Duo Mixed Leistung", "Samstag", "Mixed", "[1, 2, 3, 4]"),
 ]
+
+DEFAULT_TIEBREAK = 11
+DEFAULT_POINTS   = 15
+DEFAULT_GROUPS   = 4
+
+NAME_PREROUND = "Vorrunde Gruppenphase"
