36471687e374a7ea673b99d83b09d16a36abfff2
hein_j5 <johann.heinke@dlr.de>
2026-08-26 14:31:12 +0200

    remove old ranking function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,15 +1,7 @@
 from .auth import can_edit_match
 from .export_schedule import export_stage
 
-from .ranking import calculate_ranking, get_final_winner, get_all_final_matches, get_final_ranking_list
-
-
 __all__ = [
     # exporter
     "export_stage",
-    # ranking
-    "calculate_ranking",
-    "get_final_winner",
-    "get_all_final_matches",
-    "get_final_ranking_list",
 ]
